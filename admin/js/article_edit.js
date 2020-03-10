d4a4c7c3b5aaf0e52a763794036dec31df500751
yao <qqww@qq.com>
2020-03-10 18:14:42 +0800

    草稿和已发布封装已完成

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -42,14 +42,38 @@ $(function () {
           format: 'YYYY-MM-DD'
         });
         fuText()
+
         // 设置富文本内容的代码
-        // tinymce.activeEditor.setContent()
+        // tinymce.activeEditor.setContent()必须在此插件加载后设置，这个富文本插件比较慢
+
+        setTimeout(function () {
+          tinymce.activeEditor.setContent(content);
+        }, 2000)
         // 获取富文本内容，需要接收返回值
         // tinymce.activeEditor.getContent()
+        //图片渲染
+        $('#inputCover').change(function () {
+          console.dir(this.files[0])
+          const filesImg = this.files[0];
+          const filesUrl = URL.createObjectURL(filesImg)
+          $('.article_cover').attr({ src: filesUrl })
+        })
+        $('.btn-success').click(function (e) {
+          e.preventDefault();
+         const state = '已发布'
+         AjaxArticle(state)
+        })
       }
     }
   });
-  function fuText(){
+  $('.btn-default').click(function(e){
+    e.preventDefault();
+    const state = '草稿'
+    AjaxArticle(state);
+  })
+
+
+  function fuText() {
     tinymce.init({
       selector: '#mytextarea',
       language: 'zh_CN',
@@ -66,5 +90,32 @@ $(function () {
 
     });
   }
+function AjaxArticle( err){
+  const htmlStr = tinymce.activeEditor.getContent()
+  console.log(htmlStr);
+  const fd = new FormData(this.form);
+  console.log();
+
+  fd.append('id',bianId)
+  fd.append('content', htmlStr);
+  fd.append('state', err)
+  fd.append("date", $('#myDate').val() )
+  $.ajax({
+    type: "post",
+    url: BigNew.article_edit,
+    data: fd,
+    contentType: false,
+    processData: false,
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+      if(response.code == 200){
+        // alert('修改成功')
+        history.back();
+        // location.href= './article_edit.html'
+      }
+    }
+  });
+}
 
 })
